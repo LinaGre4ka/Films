df84a9edf7d1a8d18545a4d921f2f256f345fd56
Lina <gelyagre4ka@yandex.ru>
2024-03-17 00:23:53 +0300

    upd_fin

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,4 +1,5 @@
 import { Movie } from './Movie';
+import Cat from './cat.jpg';
 
 function Movies(props) {
     const { movies = [] } = props;
@@ -10,6 +11,9 @@ function Movies(props) {
             ) : (
                 <div class="found">
                     <h4>Not found :(</h4>
+                    <img 
+                        src={Cat} 
+                    />
                 </div>
             )}
         </div>
