8e4c5a1e8dbf94862faa9901209ca26eb3117a20
Lina <gelyagre4ka@yandex.ru>
2024-03-17 17:57:40 +0300

    updfinal

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -4,7 +4,7 @@ function Header() {
         <div className="nav-wrapper">
             <a href="!#" class="brand-logo center">React Movies</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="!#">Repository</a></li>
+                <li><a href="https://github.com/LinaGre4ka/Films">Repository</a></li>
             </ul>
         </div>
     </div>
